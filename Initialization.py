8d9870a18cc9346e857dce3a500eac8a86c703ff
neilnyn <343320904@qq.com>
2024-10-29 09:36:18 +0800

    add readme.txt

--- Initialization.py
+++ Initialization.py
@@ -4,18 +4,11 @@ from TM1py import TM1Service
 from TM1py.Objects import Dimension,Hierarchy,Element,Cube
 from collections import defaultdict
 
-# PostgreSQL连接
-conn = psycopg2.connect(
-    database="sales",
-    user="postgres",
-    password="1234",
-    host="localhost",
-    port="5432"
-)
+
 
 
 # 获取表的列名与数据
-def preview_data(table_name, limit=10):
+def preview_data(conn:'Psql Connection',table_name, limit=10):
     cursor = conn.cursor()
     cursor.execute(f"SELECT * FROM {table_name} LIMIT {limit}")
     columns = [desc[0] for desc in cursor.description]
@@ -24,16 +17,16 @@ def preview_data(table_name, limit=10):
     return columns, data
 
 # 显示数据
-def show_preview():
+def show_preview(conn:'Psql Connection'):
     tables = ["product_dimension", "store_dimension", "time_dimension", "orders"]
     selected_table = st.selectbox("选择要预览的表", tables)
-    columns, data = preview_data(selected_table)
+    columns, data = preview_data(conn,selected_table)
     st.write(f"{selected_table} 预览:")
     st.text(columns)
     st.table(data)
 
 # 通过页面选择创建维度和cube,返回维度名称，cube名称，度量值名称
-def create_drag_interface():
+def create_drag_interface(conn:'Psql Connection'):
     st.header("创建TM1维度和立方体")
 
     # 建立与数据库链接
@@ -46,24 +39,37 @@ def create_drag_interface():
     cursor = conn.cursor()
     with col1:
         st.subheader("Rows (Dimensions)")
-        dimensions = st.multiselect("选择维度", ["product_dimension", "store_dimension", "time_dimension"])
+        table_name = st.selectbox("选择维表",["product_dimension", "store_dimension", "time_dimension"])
+        dimension_name = st.text_input(label='请输入新维度名称')
         dimension_table_columns_mapping=defaultdict(list)
-        for table_name in dimensions:
-            cursor.execute(f"SELECT column_name FROM information_schema.columns WHERE table_name = '{table_name}'")
-            all_columns = [row[0] for row in cursor.fetchall()]
-            measures=st.multiselect("选择度量", [col for col in all_columns])
-            dimension_table_columns_mapping[table_name].append(measures)
+
+        cursor.execute(f"SELECT column_name FROM information_schema.columns WHERE table_name = '{table_name}'")
+        all_columns = [row[0] for row in cursor.fetchall()]
+        measures=st.multiselect("选择字段", [col for col in all_columns])
+        dimension_table_columns_mapping[table_name].append(measures)
     with col2:
         st.subheader("Columns (Measures)")
         cursor.execute("SELECT column_name FROM information_schema.columns WHERE table_name = 'orders'")
         all_columns = [row[0] for row in cursor.fetchall()]
+        measure_dimension_name = st.text_input(label='请输入Measure维度名称')
         measures = st.multiselect("选择度量",[col for col in all_columns])
-    # 关闭与数据库链接
-    cursor.close()
-    return cube_name,dimensions,dimension_table_columns_mapping, measures
+
+    return cube_name,dimension_name,dimension_table_columns_mapping, measure_dimension_name,measures
 
 # 创建维度 - 数据库适配
-def create_tm1_dimension_from_database(tm1:TM1Service,dim_name, data):
+def create_tm1_dimension_from_database(conn:'Qsql Connection',tm1:TM1Service,dim_name, dimension_mapping:dict):
+    #获取数据
+    cursor = conn.cursor()
+    table_name = dimension_mapping.keys()
+    table_name = list(table_name)[0]
+    columns = dimension_mapping.get(table_name)[0]
+    str_columns = ''
+    for col in columns:
+        str_columns = str_columns + ',' + col
+    str_columns = str_columns[1:]
+    cursor.execute(f"SELECT {str_columns} FROM {table_name}")
+    data = cursor.fetchall()
+
     # 判断维度是否存在
     if not tm1.dimensions.exists(dim_name):
         new_dimension = Dimension(dim_name)
@@ -163,44 +169,29 @@ def create_tm1_cube(tm1:TM1Service,cube_name, dimensions):
         tm1.cubes.create(new_cube)
 
 
-def create_tm1_objects(tm1:TM1Service,cube_name,dimensions,dimension_mapping:dict ,measures:list):
-    # 创建维度
-    for dim in dimensions:
-        cursor = conn.cursor()
-        columns= dimension_mapping.get(dim)[0]
-        str_columns=''
-        for col in columns:
-            str_columns = str_columns+','+col
-        str_columns = str_columns[1:]
-        cursor.execute(f"SELECT {str_columns} FROM {dim}")
-        data = cursor.fetchall()
-        cursor.close()
-        create_tm1_dimension_from_database(tm1,dim_name=dim, data=data)
 
-    # 创建度量维度
-    dim_measure_name='Measure'+ cube_name
-    create_tm1_dimension_from_list(tm1, dim_name=dim_measure_name, data=measures)
-
-    # 创建立方体
-    cube_dimensions = dimensions + [dim_measure_name]
-    create_tm1_cube(tm1,cube_name, cube_dimensions)
-
-    st.success("TM1维度和立方体创建成功!")
 
 
 def main():
+    # PostgreSQL连接
+    with psycopg2.connect(database="sales", user="postgres", password="1234", host="localhost", port="5432") as conn:
 
-    st.title("TM1 Dimension and Cube Creator")
+        show_preview(conn)
 
-    show_preview()
+        cube_name,dimension_name,dimension_table_columns_mapping, measure_dimension_name,measures = create_drag_interface()
 
-    cube_name,dimensions,dimension_table_columns_mapping, measures = create_drag_interface()
 
-    if st.button("创建TM1对象"):
-        # TM1连接
+        # TM1 Connection
         with TM1Service(address='localhost', port=8010, user='admin', password='apple', ssl=True) as tm1:
-            create_tm1_objects(tm1,cube_name,dimensions, dimension_table_columns_mapping,measures)
-
+            st.title("TM1 Dimension Creator")
+            if st.button("从Rows创建TM1维度"):
+                    create_tm1_dimension_from_database(conn, tm1, dimension_name, dimension_table_columns_mapping)
+            if st.button("从Column创建TM1维度"):
+                    create_tm1_dimension_from_list(tm1,measure_dimension_name,measures)
+            st.title("TM1 Cube Creator")
+            # 获取当前示例中的所有维度
+            all_dimensions_name = tm1.dimensions.get_all_names()
+            all_dimensions_name = [name for name in all_dimensions_name if "}" not in name]
 
 if __name__ == "__main__":
     main()
